54779efe713c9878f871ff5bfa3f7f8d8391c6f3
ThieuDinh <dinhthieubrvt@gmail.com>
2026-01-07 16:28:32 +0700

    7/1 2

--- obj/Debug/net9.0/doan_ttcn.AssemblyInfo.cs
+++ obj/Debug/net9.0/doan_ttcn.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("doan_ttcn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c32e8086f3322423cdbdfefb3ffad51b055a0f3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4a0f2d05fbde321c07a45dcb0619ae9f096c92e")]
 [assembly: System.Reflection.AssemblyProductAttribute("doan_ttcn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("doan_ttcn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
