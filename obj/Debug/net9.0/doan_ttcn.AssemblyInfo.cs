365f4b67abfee1fc0699717361946754ece6a620
ThieuDinh <dinhthieubrvt@gmail.com>
2026-03-10 21:03:00 +0700

    Luu tam thu muc obj de ep merge

--- obj/Debug/net9.0/doan_ttcn.AssemblyInfo.cs
+++ obj/Debug/net9.0/doan_ttcn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("doan_ttcn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5ec617c79a925c9327b56a88ee373d47c26df45")]
 [assembly: System.Reflection.AssemblyProductAttribute("doan_ttcn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("doan_ttcn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
